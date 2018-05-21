b1bed8bf77093a3539b38fdcf0d83858f930fd58
marleny19 <marlenymullisaca19@gmail.com>
2018-05-21 11:42:03 -0500

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColegioComponent } from './colegio/colegio.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColegioComponent,
+    ProfesoresComponent,
+    UbicacionComponent,
+    GaleriaComponent,
+    ContenidoComponent,
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
